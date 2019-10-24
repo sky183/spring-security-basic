5d92eb3638aff058fb1030d397e3af3029f655a8
sky183 <sky18333@naver.com>
2019-10-24 18:20:35 +0900

    .

--- src/main/java/me/whiteship/demospringsecurityform/account/UserAccount.java
+++ src/main/java/me/whiteship/demospringsecurityform/account/UserAccount.java
@@ -3,14 +3,14 @@ package me.whiteship.demospringsecurityform.account;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
-import java.util.List;
+import java.util.Arrays;
 
 public class UserAccount extends User {
 
     private Account account;
 
     public UserAccount(Account account) {
-        super(account.getUsername(), account.getPassword(), List.of(new SimpleGrantedAuthority("ROLE_" + account.getRole())));
+        super(account.getUsername(), account.getPassword(), Arrays.asList(new SimpleGrantedAuthority("ROLE_" + account.getRole())));
         this.account = account;
     }
 
